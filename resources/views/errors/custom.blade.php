8823ae8a6405827b2596426318a864a01a360ff3
Bernd Engels <engels@goldenacker.de>
2020-03-20 17:42:58 +0100

    corona page

--- resources/views/errors/custom.blade.php
+++ resources/views/errors/custom.blade.php
@@ -26,10 +26,11 @@
                     The consequences of the corona crisis for  Schokoalden are dramatic: no live-music, no guests, no incomes. You can save a part of Berlins live music culture by supporting Schokoladen via Paypal: spenden@schokoladen-mitte.de.
                     We'd like thank all friends from the bottom of our hearts. Viva Schokoladen!
                 </p>
-                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                <form id="frmDonate" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
                     <input type="hidden" name="cmd" value="_s-xclick" />
                     <input type="hidden" name="hosted_button_id" value="JYPGKFVHLT6YY" />
-                    <input type="image" src="https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <label for="frmImage">Please</label>
+                    <input id="frmImage" type="image" src="https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
                     <img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" />
                 </form>
             </div>
