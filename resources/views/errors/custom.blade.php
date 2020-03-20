f2471835a88db947b1d54a68544f22a6d713605a
Bernd Engels <engels@goldenacker.de>
2020-03-20 11:24:06 +0100

    corona page

--- resources/views/errors/custom.blade.php
+++ resources/views/errors/custom.blade.php
@@ -5,13 +5,34 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <h3>Aufgrund der aktuellen Lage bleiben wir geschlossen.<br>Passt auf Euch auf und bleibt gesund!</h3>
+            <h5>Aufgrund der aktuellen Lage bleiben wir geschlossen.<br>Passt auf Euch auf und bleibt gesund!</h5>
         </div>
-        <div class="row my-2">
+        <div class="row">
             <p>---------------------------------------------------------------------------------</p>
         </div>
         <div class="row">
-            <h3>due to the current situation we'll also stay closed.<br>we'll try and re-schedule all cancelled shows as soon as possible.<br>stay healthy and safe everyone.</h3>
+            <h5>due to the current situation we'll also stay closed.<br>we'll try and re-schedule all cancelled shows as soon as possible.<br>stay healthy and safe everyone.</h5>
+        </div>
+        <div class="row">
+            <p>---------------------------------------------------------------------------------</p>
+        </div>
+        <div class="row">
+            <div class="">
+                <p>
+                    Die Folgen der Koronakrise für den Schokoladen sind dramatisch: keine Live-Musik, keine Gäste, keine Einkommen. Du kannst einen Teil von Berlins Live-Musikkultur retten, indem Du den Schokoladen über Paypal unterstützt: spenden@schokoladen-mitte.de
+                    Wir danken allen Freunden von ganzem Herzen. Viva Schokoladen!
+                </p>
+                <p>
+                    The consequences of the corona crisis for  Schokoalden are dramatic: no live-music, no guests, no incomes. You can save a part of Berlins live music culture by supporting Schokoladen via Paypal: spenden@schokoladen-mitte.de.
+                    We'd like thank all friends from the bottom of our hearts. Viva Schokoladen!
+                </p>
+                <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick" />
+                    <input type="hidden" name="hosted_button_id" value="JYPGKFVHLT6YY" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/DK/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <img alt="" border="0" src="https://www.paypal.com/en_DE/i/scr/pixel.gif" width="1" height="1" />
+                </form>
+            </div>
         </div>
     </div>
 @endsection
