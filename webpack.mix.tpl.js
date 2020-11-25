c59c22a6023273681db87d87ceb9b884c89752a1
Bernd Engels <engels@goldenacker.de>
2020-11-25 17:02:03 +0100

    webpack

--- webpack.mix.tpl.js
+++ webpack.mix.tpl.js
@@ -15,8 +15,8 @@ mix.webpackConfig({
 });
 
 mix.autoload({
-        'jquery': ['jQuery', '$']
-    })
+    'jquery': ['jQuery', '$']
+})
     .setPublicPath('public_html')
     .js('resources/js/app.js', 'js/app.js')
     .js('resources/js/app-admin.js', 'js/app-admin.js')
@@ -26,7 +26,7 @@ mix.autoload({
     .copy('resources/js/admin.js', 'public_html/js')
     .copy('resources/vendor/calendar/zabuto_calendar.min.css', 'public_html/vendor/calendar/css')
     .copy('resources/vendor/calendar/zabuto_calendar.min.js', 'public_html/vendor/calendar/js')
-//    .copy('node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css.map', 'resources/vendor/bootstrap-datepicker/sass')
+    //    .copy('node_modules/bootstrap-datepicker/dist/css/bootstrap-datepicker.css.map', 'resources/vendor/bootstrap-datepicker/sass')
     .copy('node_modules/bootstrap-datepicker/dist', 'public_html/vendor/bootstrap-datepicker')
     .copy('node_modules/dropzone/dist/min/dropzone.min.js', 'public_html/vendor/dropzone/js')
     .copy('node_modules/dropzone/dist/min/dropzone.min.css', 'public_html/vendor/dropzone/css')
@@ -37,6 +37,6 @@ mix.autoload({
     .copy('node_modules/cropperjs/dist/cropper.css', 'public_html/vendor/cropperjs/css')
     .copy('resources/vendor/tinymce/plugins/responsivefilemanager', 'public_html/vendor/tinymce/plugins/responsivefilemanager')
     .copy('resources/vendor/tinymce/filemanager', 'public_html/vendor/filemanager')
-    .copy('vendor/recca0120/terminal/public', 'public_html/vendor/terminal')
+    //    .copy('vendor/recca0120/terminal/public', 'public_html/vendor/terminal')
     .copy('node_modules/leaflet/dist', 'public_html/vendor/leaflet')
 ;
