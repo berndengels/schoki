a142a168bf5086d62d156f061f67a170167e9763
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-12-10 18:52:31 +0100

    remove asset function, take mix

--- resources/views/public/templates/topNavigation.blade.php
+++ resources/views/public/templates/topNavigation.blade.php
@@ -1,8 +1,8 @@
 <div id="top-navigation">
     <nav class="navbar fixed-top navbar-expand-md navbar-dark bg-black">
         <a class="navbar-brand" href="/events">
-            <img src="{{ mix('img/batcow_yellow.png') }}">
-            <img src="{{ mix('img/schokoladen_schrift_yellow.png') }}"></a>
+            <img src="{{ asset('img/batcow_yellow.png') }}">
+            <img src="{{ asset('img/schokoladen_schrift_yellow.png') }}"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#topNavbar" aria-controls="topNavbar" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -30,7 +30,7 @@
         </div>
 
         <div class="d-sm-none d-lg-block float-right my-auto mr-3">
-            <img src="{{ mix('img/power/address.png') }}" width="183" height="14" alt="Schokoladen, Ackerstraße 169, 10115 Berlin" title="Schokoladen, Ackerstraße 169, 10115 Berlin" />
+            <img src="{{ asset('img/power/address.png') }}" width="183" height="14" alt="Schokoladen, Ackerstraße 169, 10115 Berlin" title="Schokoladen, Ackerstraße 169, 10115 Berlin" />
         </div>
     </nav>
 </div>
