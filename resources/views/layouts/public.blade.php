f693e9c6657057fbfc3ac7e55a148347434a5b35
berndengels <99887389+bta-engels@users.noreply.github.com>
2022-12-12 19:33:16 +0100

    fixes

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -7,7 +7,6 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="robots" content="index, follow" />
-    <meta http-equiv="pragma" content="no-cache" />
     <meta http-equiv="expires" content="3600" />
     <meta http-equiv="cache-control" content="max-age=3600">
     <meta name="description" content="Schokoladen-Mitte Berlin" />
