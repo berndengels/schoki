bf105f8566011496d3c60f8adca6ab45d70a0bc8
bengels <technik@bta-weiterbildung.de>
2024-11-08 20:20:45 +0100

    file permíssions

--- app/Helper/FilePermissions.php
+++ app/Helper/FilePermissions.php
@@ -40,11 +40,11 @@ class FilePermissions {
 		$basePath = App::basePath();
 		echo "<h3>Set Permissions on $basePath</h3>";
 
-		$testPath = realpath("{$basePath}/../test.schokoladen-mitte.de/repo");
+		$testPath = realpath("/web/schokoladen-mitte.de/test.schokoladen-mitte.de/repo");
 		echo "<h5>$testPath</h5>";
 		ob_flush();
 		flush();
-
+/*
 		$output = system("find $testPath -type d -exec chmod 0777 {} +");
 		echo "Dirs: $output<br>";
 		ob_flush();
@@ -54,7 +54,7 @@ class FilePermissions {
 		echo "Files: $output<br>";
 		ob_flush();
 		flush();
-
+*/
 		foreach(self::$writableDirs as $dir) {
 			$fullPath = "$basePath/$dir/";
 			echo "<h5>$fullPath</h5>";
