ccadf906b54ebc9dd4f7b4f704d4d0628ae8bb59
bengels <technik@bta-weiterbildung.de>
2024-11-11 16:48:23 +0100

    deploy

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,23 +1,18 @@
-@servers(['schoki' => 'schoki@schokoladen-mitte.de'])
+@servers(['prod' => 'schoki@schokoladen-mitte.de','test' => 'goldenacker@goldenacker.de'])
 
-@setup
-	$npm = './npm.sh';
-@endsetup
-
-@task('test', ['on' => 'schoki'])
+@task('test', ['on' => 'test'])
 echo "testing...";
-cd ./web/schokoladen-mitte.de/test.schokoladen-mitte.de/repo
+cd ./schoki.goldenacker.de
 pwd
 git config pull.rebase false
 git pull origin frontend
-{{ $npm }} run prod
+npm run prod
 @endtask
 
-@task('prod', ['on' => 'schoki'])
+@task('prod', ['on' => 'prod'])
 echo "prod...";
 cd ./web/schokoladen-mitte.de/www.schokoladen-mitte.de
 pwd
 git config pull.rebase false
 git pull origin master
-{{ $npm }} run prod
 @endtask
